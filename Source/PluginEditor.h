557f9c46bdde3fbc7a4da662a0dcbea99ce2a9ae
muhammadaidildzulfikarsaviola-ui <muhammadaidildzulfikarsaviola@gmail.com>
2026-09-16 20:25:56 +0700

    fix: prepare responsive editor state

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,6 +2,19 @@
 #include "PluginProcessor.h"
 class MaDriveAudioProcessorEditor final : public juce::AudioProcessorEditor, private juce::Timer
 {
-public: explicit MaDriveAudioProcessorEditor(MaDriveAudioProcessor&); ~MaDriveAudioProcessorEditor() override; void paint(juce::Graphics&) override; void resized() override;
-private: void timerCallback() override { repaint(); } MaDriveAudioProcessor& p; juce::OwnedArray<juce::Slider> knobs; juce::OwnedArray<juce::Label> labels; juce::OwnedArray<juce::TextButton> buttons; std::vector<std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>> attachments; std::vector<std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment>> buttonAttachments; JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MaDriveAudioProcessorEditor)
+public:
+    explicit MaDriveAudioProcessorEditor(MaDriveAudioProcessor&);
+    ~MaDriveAudioProcessorEditor() override;
+    void paint(juce::Graphics&) override;
+    void resized() override;
+private:
+    void timerCallback() override { repaint(); }
+    MaDriveAudioProcessor& p;
+    juce::OwnedArray<juce::Slider> knobs;
+    juce::OwnedArray<juce::Label> labels;
+    juce::OwnedArray<juce::TextButton> buttons;
+    std::vector<std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>> attachments;
+    std::vector<std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment>> buttonAttachments;
+    bool activeB = false;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MaDriveAudioProcessorEditor)
 };
